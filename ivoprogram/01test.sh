b865191a94e56d7c47bc6066eb71e3d0bb4351d5
Ivo Gjorgjievski <ivoprogram@mail.ru>
2019-11-07 12:25:08 +0100

    Version 1.4.2 2019-11-07

---
+++ ivoprogram/01test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+DIR='/tmp/01test'
+
+echo
+echo "Creating directories"
+mkdir $DIR/
+mkdir $DIR/dir1\\
+mkdir $DIR/dir2\"
+mkdir $DIR/dir3:
+
+echo "Overwriting in directories"
+overwrite -test -dirs:1 -data:1mb -path:$DIR/dir1\\
+overwrite -test -dirs:1 -data:1mb -path:$DIR/dir2\"
+overwrite -test -dirs:1 -data:1mb -path:$DIR/dir3:
+
+echo
+echo "Result in $DIR/dir1\\"
+ls -lh $DIR/dir1\\
+
+echo
+echo "Result in $DIR/dir2\" "
+ls -lh $DIR/dir2\"
+
+echo
+echo "Result in $DIR/dir3:"
+ls -lh $DIR/dir3:
+
+echo
+echo "Cleaning"
+rm -r $DIR
+
+echo "Done."
